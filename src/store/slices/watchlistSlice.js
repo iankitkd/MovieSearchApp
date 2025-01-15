c349ed252ab4552f5eb816f5d370a7c3428ad033
Ankit Kumar <ankitkr.9105@gmail.com>
2025-01-16 00:15:59 +0530

    Add feature for watchlist saving to local storage

--- src/store/slices/watchlistSlice.js
+++ src/store/slices/watchlistSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    contents: [],
+    contents: JSON.parse(localStorage.getItem('watchlist')) || [],
 }
 
 const watchlistSlice = createSlice({
@@ -12,12 +12,14 @@ const watchlistSlice = createSlice({
             const content = action.payload;
             if(!state.contents.some(c => c.id === content.id)) {
                 state.contents.push(content);
+                localStorage.setItem('watchlist', JSON.stringify(state.contents));
             }
         },
 
         removeFromWatchlist: (state, action) => {
             const contentId = action.payload;
             state.contents = state.contents.filter(c => c.id !== contentId);
+            localStorage.setItem('watchlist', JSON.stringify(state.contents)); 
         }
     }
 })
